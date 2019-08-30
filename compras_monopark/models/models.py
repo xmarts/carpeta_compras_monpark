f24ea6d1c8cdfb3ee4a9e8f8ce5fa9fa26d07820
gilxmarts <gilberto.santiago@xmarts.com>
2019-08-30 13:45:16 -0500

    Imagen en tabla de compras

--- compras_monopark/models/models.py
+++ compras_monopark/models/models.py
@@ -6,3 +6,14 @@ class PurchaseOrder(models.Model):
 	_inherit = "purchase.order"
 
 	compra_tipo = fields.Selection(selection=[('nacional', 'Nacional'),('internacional', 'Internacional')], string="Tipo de compra")
+
+class PurchaseOrderLine(models.Model):
+	_inherit = "purchase.order.line"
+
+	imagen_producto = fields.Binary(compute="_get_imagen")
+
+	@api.depends('product_id')
+	def _get_imagen(self):
+		for line in self:
+			if line.product_id:
+				line.imagen_producto = line.product_id.image_medium
